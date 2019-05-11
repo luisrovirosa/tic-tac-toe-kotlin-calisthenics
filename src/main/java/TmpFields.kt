e78c48e76a919a604b267bca3d068500c91b71de
Luis Rovirosa <luisrovirosa@gmail.com>
2019-05-11 09:52:48 +0200

    ignore

--- src/main/java/TmpFields.kt
+++ src/main/java/TmpFields.kt
@@ -1,13 +1,13 @@
 class TmpFields(val value: Map<Position, Field>) {
+    fun belongsAllTo(player: Player): Boolean {
+        return value.all { it -> it.value.belongsTo(player) }
+    }
+
     fun row(rowNumber: Int): TmpFields {
         return TmpFields(value.filterKeys { position -> position.inRow(rowNumber) })
     }
 
     fun col(columnNumber: Int): TmpFields {
         return TmpFields(value.filterKeys { position -> position.inColumn(columnNumber) })
-    }
-
-    fun belongsAllTo(player: Player): Boolean {
-        return value.all { it -> it.value.belongsTo(player) }
     }
 }
